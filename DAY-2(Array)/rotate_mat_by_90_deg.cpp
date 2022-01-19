b0e183396a346476c535ace280d1d5cca6d01f91
Aindrail <aindrailsantra@gmail.com>
2022-01-19 18:54:52 +0530

    Day 2 stated.

---
+++ DAY-2(Array)/rotate_mat_by_90_deg.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for( int i=0 ; i<n; i++){
+            for(int j=0;j<i;j++)
+   swap(matrix[i][j],matrix[j][i]);
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
+//Q link :- https://leetcode.com/problems/rotate-image/submissions/
+// Logic :- 90 degree rotation is Transpose(arr[i][j]=arr[j][i]) and reverse 
