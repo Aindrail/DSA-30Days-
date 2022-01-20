88e6b9787242bd22512df7b5cf3e082fd0699993
Aindrail <aindrailsantra@gmail.com>
2022-01-20 18:26:34 +0530

    Use (sum)N2-(sum)X2 and sum(N) and sum(X) to find missing and repeating no

--- DAY-2(Array)/find_missing_and_repeating.cpp
+++ DAY-2(Array)/find_missing_and_repeating.cpp
@@ -1,23 +1,31 @@
 vector<int> find_missing_repeating(vector<int> array)
 {
-    int n = array.size() + 1;
+  vector<int>missing_repeated_number(const vector<int> &A) {
+    long long int len = A.size();
 
-    vector<int> substitute(n, 0); // initializing the substitute array with 0 of size n+1.
-
-    vector<int> ans;              // initializing the answer  array .
-
-    for (int i = 0; i < array.size(); i++)
-    {
-        substitute[array[i]]++;
+    long long int S = (len * (len+1) ) /2;
+    long long int P = (len * (len +1) *(2*len +1) )/6;
+    long long int missingNumber=0, repeating=0;
+     
+    for(int i=0;i<A.size(); i++){
+       S -= (long long int)A[i];
+       P -= (long long int)A[i]*(long long int)A[i];
     }
+     
+    missingNumber = (S + P/S)/2;
 
-    for (int i = 1; i <= array.size(); i++)
-    {
-        if (substitute[i] == 0 || substitute[i] > 1)
-        {
-            ans.push_back(i);
-        }
-    }
+    repeating = missingNumber - S;
+
+    vector <int> ans;
+
+    ans.push_back(repeating);
+    ans.push_back(missingNumber);
 
     return ans;
 }
+
+// Q link :- https://www.interviewbit.com/problems/repeat-and-missing-number-array/
+// Approach :- Find summ of X2 and summation of all element in array. Then subtract with suma n and summ n2.   A2-B2=(A+b) (A-b) we have A+B so we can get A-B. no add A+b and A-b = 2A=x(let) A=x/2 then find B. where A is missing and B is extra.
+// TC :- O(n) SC :-O(1)
+// Appraoch 2:- use x or. find rigthmost bit of XOR value. seperate all element in two buckets on that basis. then seperate all no till N(no of elements) in two buckets . and takes Xor of each element in the respective buckets . then iterate and find which is missing and which is extra
+// TC :- O(n)  SC :- O(1)
