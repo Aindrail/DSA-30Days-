c8afa1e219dc6f056d910f90bf53990930dac43f
Aindrail <aindrailsantra@gmail.com>
2022-01-21 23:01:53 +0530

    Push all in hash set and search for consecutive by searching the array

---
+++ DAY-4(Array)/longest_con_sequence.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int longestConsecutive(vector < int > & nums) {
+  set < int > hashSet;
+  for (int num: nums) {
+    hashSet.insert(num);
+  }
+
+  int longestStreak = 0;
+
+  for (int num: nums) {
+    if (!hashSet.count(num - 1)) {
+      int currentNum = num;
+      int currentStreak = 1;
+
+      while (hashSet.count(currentNum + 1)) {
+        currentNum += 1;
+        currentStreak += 1;
+      }
+
+      longestStreak = max(longestStreak, currentStreak);
+    }
+  }
+
+  return longestStreak;
+}
+int main() {
+  vector<int> arr{100,200,1,2,3,4};
+  int lon = longestConsecutive(arr);
+  cout << "The longest consecutive sequence is " << lon << endl;
+
+}
+//Qlink :- https://leetcode.com/problems/longest-consecutive-sequence/
+// TC :- O(N)  SC :- O(N)
+//Approach: We will first push all are elements in the HashSet. Then we will run a for loop and check for any number(x) if it is the starting number of the consecutive sequence by checking if the HashSet contains (x-1) or not. If ‘x’ is the starting number of the consecutive sequence we will keep searching for the numbers y = x+1, x+2, x+3, ….. And stop at the first ‘y’ which is not present in the HashSet. Using this we can calculate the length of the longest consecutive subsequence. 
