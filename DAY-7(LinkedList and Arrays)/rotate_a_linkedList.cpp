4d49194105182fefcfa57b1190be9178eb2a677c
Aindrail <aindrailsantra@gmail.com>
2022-02-05 01:54:38 +0530

    make it circular and point head and null in position

---
+++ DAY-7(LinkedList and Arrays)/rotate_a_linkedList.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL || head->next==NULL)return head;
+        int  length =1;
+        ListNode *temp = head;
+        while(temp->next!=NULL && ++length)
+temp= temp->next;
+        temp->next=head;
+        k=k%length;
+        k=length-k;
+        while(k--){
+temp=temp->next;
+        }
+        head=temp->next;
+        temp->next=NULL;
+        return head;
+    }
+};
+//Qlink :- https://leetcode.com/problems/rotate-list/description/
+//Approach :- covert to circular lll and find the length then iterate len-k+1 and make next as head and ->next as head.
+//TC :- O(n)+O(n-(n%k)) 
