fc249e1a81292d346c964b0a0f891a98a7863873
Aindrail <aindrailsantra@gmail.com>
2022-02-19 22:28:46 +0530

    Sort both array and compare arr and dep time

---
+++ DAY-8(Greedy Algo)/train_station_req.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    
+    
+    
+    int findPlatform(int a[], int d[], int n)
+    {
+    	// Your code here
+    int plat =1;
+    sort(a, a+n); sort(d, d+n);
+    int i=1; int j=0;
+    int ans=1;
+    while(i<n && j<n){
+       if(a[i] <= d[j]){
+           plat++;
+           i++;
+       } 
+       else if(a[i] > d[j]){
+           plat--;
+            j++;
+       }
+       ans= max(plat,ans);
+    }
+    
+    return ans;
+    
+    }
+};
+
+
+// { Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}  // } Driver Code Ends
