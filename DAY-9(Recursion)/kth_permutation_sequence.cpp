aa8ec2d9da5544c8bf41fd189f34af13ae653465
Aindrail <aindrailsantra@gmail.com>
2022-03-22 19:49:04 +0530

    Use dictionary intuition

--- DAY-9(Recursion)/kth_permutation_sequence.cpp
+++ DAY-9(Recursion)/kth_permutation_sequence.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 class Solution {
   public:
-    //function to generate all possible permutations of a string
-    // void solve(string & s, int index, vector < string > & res) {
-    //   if (index == s.size()) {
-    //     res.push_back(s);
-    //     return;
-    //   }
-    //   for (int i = index; i < s.size(); i++) {
-    //     swap(s[i], s[index]);
-    //     solve(s, index + 1, res);
-    //     swap(s[i], s[index]);
-    //   }
-    // }
+    function to generate all possible permutations of a string
+    void solve(string & s, int index, vector < string > & res) {
+      if (index == s.size()) {
+        res.push_back(s);
+        return;
+      }
+      for (int i = index; i < s.size(); i++) {
+        swap(s[i], s[index]);
+        solve(s, index + 1, res);
+        swap(s[i], s[index]);
+      }
+    }
 
   string getPermutation(int n, int k) {
     string s;
