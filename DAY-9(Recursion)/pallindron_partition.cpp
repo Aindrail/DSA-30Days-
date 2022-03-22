f8b650d73c529f31fb4509adb4215f66fb30624d
Aindrail <aindrailsantra@gmail.com>
2022-03-22 19:51:33 +0530

    Generated substring by recursion

--- DAY-9(Recursion)/pallindron_partition.cpp
+++ DAY-9(Recursion)/pallindron_partition.cpp
@@ -53,10 +53,10 @@ int main() {
   return 0;
 }
 
-// Time Complexity: O( (2^n) *k*(n/2) )
+Time Complexity: O( (2^n) *k*(n/2) )
 
-// Reason: O(2^n) to generate every substring and O(n/2)  to check if the substring generated is a palindrome. O(k) is for inserting the palindromes in another data structure, where k  is the average length of the palindrome list.
+Reason: O(2^n) to generate every substring and O(n/2)  to check if the substring generated is a palindrome. O(k) is for inserting the palindromes in another data structure, where k  is the average length of the palindrome list.
 
-// Space Complexity: O(k * x)
+Space Complexity: O(k * x)
 
-// Reason: The space complexity can vary depending upon the length of the answer. k is the average length of the list of palindromes and if we have x such list of palindromes in our final answer. The depth of the recursion tree is n, so the auxiliary space required is equal to the O(n).
+Reason: The space complexity can vary depending upon the length of the answer. k is the average length of the list of palindromes and if we have x such list of palindromes in our final answer. The depth of the recursion tree is n, so the auxiliary space required is equal to the O(n).
