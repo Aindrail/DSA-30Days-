7627806ff2b30ddc89c3b31439781d6faeef5761
Aindrail <aindrailsantra@gmail.com>
2022-03-22 19:51:15 +0530

    Generated substring by recursion

--- DAY-9(Recursion)/pallindron_partition.cpp
+++ DAY-9(Recursion)/pallindron_partition.cpp
@@ -11,20 +11,20 @@ class Solution {
       return res;
     }
 
-  // void func(int index, string s, vector < string > & path,
-  //   vector < vector < string > > & res) {
-  //   if (index == s.size()) {
-  //     res.push_back(path);
-  //     return;
-  //   }
-  //   for (int i = index; i < s.size(); ++i) {
-  //     if (isPalindrome(s, index, i)) {
-  //       path.push_back(s.substr(index, i - index + 1));
-  //       func(i + 1, s, path, res);
-  //       path.pop_back();
-  //     }
-  //   }
-  // }
+  void func(int index, string s, vector < string > & path,
+    vector < vector < string > > & res) {
+    if (index == s.size()) {
+      res.push_back(path);
+      return;
+    }
+    for (int i = index; i < s.size(); ++i) {
+      if (isPalindrome(s, index, i)) {
+        path.push_back(s.substr(index, i - index + 1));
+        func(i + 1, s, path, res);
+        path.pop_back();
+      }
+    }
+  }
 
   bool isPalindrome(string s, int start, int end) {
     while (start <= end) {
