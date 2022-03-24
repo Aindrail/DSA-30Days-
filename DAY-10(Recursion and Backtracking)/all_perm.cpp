1fdde8ed244914087673866f7c6b39262e01544f
Aindrail <aindrailsantra@gmail.com>
2022-03-24 19:01:50 +0530

    Use recursion and backtracking swap and unswap

--- DAY-10(Recursion and Backtracking)/all_perm.cpp
+++ DAY-10(Recursion and Backtracking)/all_perm.cpp
@@ -37,3 +37,6 @@ int main() {
 //Qlink :- leetcode.com/problems/permutations/
 
 //Approach :- Swap  recurse  unswap . take a for loop that goes from index to size. recurse return index+1 and swap nums[ind] and num[i]
+//Time Complexity: O(N! X N)
+
+Space Complexity: O(1)
