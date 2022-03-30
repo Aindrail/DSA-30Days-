8ffd34d0d2e762c90e22411acaaf359c95f1d1d4
Aindrail <aindrailsantra@gmail.com>
2022-03-30 23:26:57 +0530

    Use recursion and backtracking

--- DAY-10(Recursion and Backtracking)/N_Queen.cpp
+++ DAY-10(Recursion and Backtracking)/N_Queen.cpp
@@ -35,19 +35,19 @@ class Solution {
     }
 
   public:
-    void solve(int col, vector < string > & board, vector < vector < string >> & ans, int n) {
-      if (col == n) {
-        ans.push_back(board);
-        return;
-      }
-      for (int row = 0; row < n; row++) {
-        if (isSafe1(row, col, board, n)) {
-          board[row][col] = 'Q';
-          solve(col + 1, board, ans, n);
-          board[row][col] = '.';
-        }
-      }
-    }
+    // void solve(int col, vector < string > & board, vector < vector < string >> & ans, int n) {
+    //   if (col == n) {
+    //     ans.push_back(board);
+    //     return;
+    //   }
+    //   for (int row = 0; row < n; row++) {
+    //     if (isSafe1(row, col, board, n)) {
+    //       board[row][col] = 'Q';
+    //       solve(col + 1, board, ans, n);
+    //       board[row][col] = '.';
+    //     }
+    //   }
+    // }
 
   public:
     vector < vector < string >> solveNQueens(int n) {
