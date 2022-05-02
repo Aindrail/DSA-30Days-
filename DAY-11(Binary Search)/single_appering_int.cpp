f238d8bc0ceb33a8604b4d49c9b713161c1a64a2
Aindrail <aindrailsantra@gmail.com>
2022-05-02 21:59:36 +0530

    when a single will appear then index of repeating int will be even

---
+++ DAY-11(Binary Search)/single_appering_int.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l =0 , r = nums.size()-1;
+        if(nums.size()==1){
+            return nums[0];
+        }
+        else{
+            while(l<=r){
+            int mid = (l+r)>>1;
+            if(mid%2==0){
+                if(nums[mid]==nums[mid+1]){
+                    l = mid+1;
+                }
+                else
+                    r = mid-1;
+            }
+            else{
+              if(nums[mid]==nums[mid-1]){
+                    l = mid+1;
+                }
+                else
+                    r = mid-1;
+            }
+        }
+        }
+        
+        return nums[l];
+        
+        
+    }
+};
