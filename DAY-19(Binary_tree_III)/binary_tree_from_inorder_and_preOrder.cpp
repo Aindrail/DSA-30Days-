07cff31a81f6c29cb567aaa5e7b3ed6bbbf10920
Aindrail <aindrailsantra@gmail.com>
2022-06-14 12:42:31 +0530

    Divide it into smaller problem and take recursion

--- DAY-19(Binary_tree_III)/binary_tree_from_inorder_and_preOrder.cpp
+++ DAY-19(Binary_tree_III)/binary_tree_from_inorder_and_preOrder.cpp
@@ -1,20 +1,20 @@
 class Solution {
-    TreeNode* buildTree(vector<int>& preorder,int preStart,int preEnd,vector<int>& inorder,int inStart, int inEnd , unordered_map<int,int> &inMap){
-        if(preStart>preEnd || inStart>inEnd) return NULL;
-        TreeNode* node = new TreeNode(preorder[preStart]);
+    TreeNode* buildTree(vector<int>& postorder,int postStart,int postEnd,vector<int>& inorder,int inStart, int inEnd , unordered_map<int,int> &inMap){
+        if(postStart>postEnd || inStart>inEnd) return NULL;
+        TreeNode* node = new TreeNode(postorder[postStart]);
         int inRoot = inMap[node->val];
         int size = inRoot-inStart;
-        node->left = buildTree(preorder,preStart+1,preStart+size,inorder,inStart,inRoot-1,inMap);
-        node->right = buildTree(preorder,preStart+size+1,preEnd,inorder,inRoot+1,inEnd,inMap);
+        node->left = buildTree(postorder,postStart+1,postStart+size,inorder,inStart,inRoot-1,inMap);
+        node->right = buildTree(postorder,postStart+size+1,postEnd,inorder,inRoot+1,inEnd,inMap);
         return node;
     }
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    TreeNode* buildTree(vector<int>& postorder, vector<int>& inorder) {
         unordered_map<int,int> inMap;
         for(int i = 0 ; i<inorder.size(); i++){
              inMap[inorder[i]] = i;
         }
-        TreeNode* root = buildTree(preorder,0,preorder.size()-1,inorder,0, inorder.size()-1 , inMap);
+        TreeNode* root = buildTree(postorder,0,postorder.size()-1,inorder,0, inorder.size()-1 , inMap);
         return root;
     }
 };
