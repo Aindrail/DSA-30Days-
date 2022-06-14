8dd036531bb32c6852d5541af75bd24d1260abe9
Aindrail <aindrailsantra@gmail.com>
2022-06-14 23:08:08 +0530

    check left is valid and right is valid having condition if root val is > max or root cal is < min return false

---
+++ DAY-20(Binary Search Tree)/valid_BST.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    bool isValid(TreeNode* root, long min , long max){
+         if(root==NULL) return true;
+        if(root->val>=max || root->val <=min ) return false;
+        return isValid(root->left,min,root->val) && isValid(root->right,root->val,max);
+        
+
+    }
+public:
+    bool isValidBST(TreeNode* root) {
+        if(root->left==NULL && root->right==NULL) return true;
+       
+        return isValid(root , LONG_MIN, LONG_MAX);
+        
+    }
+};
